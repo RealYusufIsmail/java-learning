168944146054ff9f299181a25a7b55b6b49c7bd1
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-20 21:37:47 +0100

    Create Main.java

---
+++ java8/::/Main.java
@@ -0,0 +1,10 @@
+//The restriction: the class should have the name Main
+public class Main {
+
+	//The start point of your program
+	public static void main(String[] args) {
+		System.out.println("It compiles!"); //It prints "It compiles!"
+
+		//Best option for this seneraro is to use //
+	}
+}
