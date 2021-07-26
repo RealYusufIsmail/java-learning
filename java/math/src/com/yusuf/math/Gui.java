9e594abfff993e9ac9b8f2a4ae121593930115cb
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-26 16:03:50 +0100

    Create Gui.java

---
+++ java/math/src/com/yusuf/math/Gui.java
@@ -0,0 +1,18 @@
+package com.yusuf.math;
+
+import javax.swing.JOptionPane;
+
+public class Gui {
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("Enter thy name;");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter thy age")); //use this when you are using an integer(int).
+        JOptionPane.showMessageDialog(null, "Thy is " + age + " years old");
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter thy height in cm")); //use this when you are using an double.
+        JOptionPane.showMessageDialog(null, "Thy is " + height + " cm tall");
+
+    }
+}
