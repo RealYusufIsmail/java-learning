19bd35d251b9573651d64c54ea8bdebc7c7bb913
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-26 20:05:53 +0100

    Create Loop.java

---
+++ java/learning/src/com/yusuf/learning/Loop.java
@@ -0,0 +1,14 @@
+package com.yusuf.learning;
+
+public class Loop {
+    public static void main(String[] args) {
+
+        // for loop = executes a block of code a limited amount of times
+
+        for(int i = 0; i <= 10; i++) {
+            //i short for index
+            System.out.println(i);
+        }
+
+    }
+}
