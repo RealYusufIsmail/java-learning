684aef1ef7cc0f081bebbf8bb1d95d94f2d5d146
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-26 13:25:15 +0100

    update

---
+++ java/math/src/com/yusuf/math/Main.java
@@ -0,0 +1,12 @@
+package com.yusuf.math;
+
+public class Main {
+
+    public static void main(String[] args) {
+	     int y = 2000;
+	     double x = 3.212;
+	     boolean easy = true;
+	     char hash = '#';
+        System.out.println(hash);
+    }
+}
---
+++ java/math/src/com/yusuf/math/SwapTwoVariables.java
@@ -0,0 +1,16 @@
+package com.yusuf.math;
+
+public class SwapTwoVariables {
+    public static void main(String[] args) {
+        String x = "Water";
+        String y = "Apple Juice";
+        String temp;
+
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("x: " +x);
+        System.out.println("y: " +y);
+    }
+}
