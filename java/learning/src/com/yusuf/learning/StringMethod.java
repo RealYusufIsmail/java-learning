f2daaa9734c056b6cbe71d1a3e44494f25639478
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-27 10:47:20 +0100

    Create StringMethod.java

---
+++ java/learning/src/com/yusuf/learning/StringMethod.java
@@ -0,0 +1,9 @@
+package com.yusuf.learning;
+
+public class StringMethod {
+    public static void main(String[] args) {
+        // String = a reference data type that can store one or more characters
+        //   reference data types have access to useful methods
+        
+    }
+}
