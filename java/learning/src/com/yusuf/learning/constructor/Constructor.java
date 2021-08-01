85f13be93886f30ef5ca8f620e756e76e0a901fa
YusufIsmail <yusufgamer222@gmail.com>
2021-08-01 22:07:30 +0100

    pause

---
+++ java/learning/src/com/yusuf/learning/constructor/Constructor.java
@@ -0,0 +1,8 @@
+package com.yusuf.learning.constructor;
+
+public class Constructor {
+    public static void main(String[] args) {
+
+        Human human = new Human("Yusuf",14,57.5,5.3);
+    }
+}
---
+++ java/learning/src/com/yusuf/learning/constructor/Human.java
@@ -0,0 +1,14 @@
+package com.yusuf.learning.constructor;
+
+public class Human {
+
+    String name;
+    int age;
+    double weight;
+    double height;
+
+    Human(String name, int age, double weight, double height) {
+
+    }
+
+}
