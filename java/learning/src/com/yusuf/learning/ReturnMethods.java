3bd2ae972f69db012921a0c17367a9a8bb59ca55
Yusuf <yusufgamer222@gmail.com>
2021-08-01 19:03:30 +0100

    Finished method lesson

---
+++ java/learning/src/com/yusuf/learning/ReturnMethods.java
@@ -0,0 +1,21 @@
+package com.yusuf.learning;
+
+public class ReturnMethods {
+    public static void main(String[] args) {
+        // method = a block of code that is executed whenever it is called upon
+
+        int x = 4;
+        int y = 3;
+
+        //this is a local variable
+        //int z = add(x,y);
+
+        //another way to do it
+        System.out.println(add(x,y));
+    }
+
+    public static int add(int x, int y) {
+        int z = x + y;
+        return z;
+    }
+}
