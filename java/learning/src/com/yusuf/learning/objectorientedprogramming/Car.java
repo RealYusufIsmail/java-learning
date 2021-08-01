c545364f5e856b2c671d80c9e51cb64161a76e39
YusufIsmail <yusufgamer222@gmail.com>
2021-08-01 21:56:01 +0100

    finished this lesson

---
+++ java/learning/src/com/yusuf/learning/objectorientedprogramming/Car.java
@@ -0,0 +1,18 @@
+package com.yusuf.learning.objectorientedprogramming;
+
+public class Car {
+
+    String make = "Tesla";
+    String model = "Model x";
+    int year = 2020;
+    String colour = "black";
+    double price = 60000.0;
+
+    public void drive() {
+        System.out.println("You drive the car");
+    }
+
+    public void brake() {
+        System.out.println("You have stepped on the brakes");
+    }
+}
