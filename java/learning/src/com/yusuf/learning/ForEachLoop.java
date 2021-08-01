d71aea473954c0cf2b206f702cd54eca07348b40
Yusuf <yusufgamer222@gmail.com>
2021-08-01 16:23:26 +0100

    Create ForEachLoop.java

---
+++ java/learning/src/com/yusuf/learning/ForEachLoop.java
@@ -0,0 +1,8 @@
+package com.yusuf.learning;
+
+public class ForEachLoop {
+    // for-each = 	traversing technique to iterate through the elements in an array/collection
+    //				less steps, more readable
+    //				less flexible
+
+}
