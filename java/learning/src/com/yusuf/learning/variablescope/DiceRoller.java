4a2aec3b1f5fd5d36a7ef92d3bc186270012ac63
YusufIsmail <yusufgamer222@gmail.com>
2021-08-02 15:23:13 +0100

    finished this lesson

---
+++ java/learning/src/com/yusuf/learning/variablescope/DiceRoller.java
@@ -0,0 +1,18 @@
+package com.yusuf.learning.variablescope;
+
+import java.util.Random;
+
+public class DiceRoller {
+    Random random;
+    int number; // this is global.
+
+    DiceRoller() {
+        random = new Random();
+        roll();
+    }
+
+    public void roll() {
+            number = random.nextInt(6)+1;
+            System.out.println(number);
+    }
+}
---
+++ java/learning/src/com/yusuf/learning/variablescope/VariableScope.java
@@ -0,0 +1,13 @@
+package com.yusuf.learning.variablescope;
+
+public class VariableScope {
+    public static void main(String[] args) {
+        //local =  declared inside a method
+        //   visible only to that method
+
+        //global  = declared outside a method, but within a class
+        //   visible to all parts of a class
+
+        DiceRoller DiceRoller = new DiceRoller();
+    }
+}
