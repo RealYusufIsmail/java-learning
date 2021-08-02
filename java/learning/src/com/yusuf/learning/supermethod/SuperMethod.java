bd553234579ef6af7b6d99edd4f463ff0e828013
YusufIsmail <yusufgamer222@gmail.com>
2021-08-02 18:54:34 +0100

    Finished this lesson

---
+++ java/learning/src/com/yusuf/learning/supermethod/SuperMethod.java
@@ -0,0 +1,13 @@
+package com.yusuf.learning.supermethod;
+
+public class SuperMethod {
+    public static void main(String[] args) {
+        // super = 	keyword refers to the superclass (parent) of an object
+        //			very similar to the "this" keyword
+
+        Hero hero1 = new Hero("Batman", 42, "Money");
+        Hero hero2 = new Hero("Superman", 40, "Everything");
+
+        System.out.println(hero2.toString());
+    }
+}
