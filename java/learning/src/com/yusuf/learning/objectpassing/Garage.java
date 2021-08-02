1bd637d0729b411169d8d88d7b95ad60a63ae655
YusufIsmail <yusufgamer222@gmail.com>
2021-08-02 16:01:33 +0100

    Finished this lesson

---
+++ java/learning/src/com/yusuf/learning/objectpassing/Garage.java
@@ -0,0 +1,9 @@
+package com.yusuf.learning.objectpassing;
+
+public class Garage {
+
+    public void park(Car car) {
+        System.out.println("The " +car.name+ " is parked in the garage.");
+    }
+
+}
