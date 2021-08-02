997faab2bdb320c04a111d2911398643a4788dc4
YusufIsmail <yusufgamer222@gmail.com>
2021-08-02 13:24:57 +0100

    finished this lesson

--- java/learning/src/com/yusuf/learning/constructor/Human.java
+++ java/learning/src/com/yusuf/learning/constructor/Human.java
@@ -8,7 +8,19 @@ public class Human {
     double height;
 
     Human(String name, int age, double weight, double height) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.height = height;
 
+    }
+
+    public void eat() {
+        System.out.println(this.name + " is eating");
+    }
+
+    public void drink() {
+        System.out.println(this.name + " is drinking");
     }
 
 }
