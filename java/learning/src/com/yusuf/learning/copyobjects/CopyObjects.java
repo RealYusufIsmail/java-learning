ecf1d5002cab570459bdd95b9f230abbc774bde8
YusufIsmail <yusufgamer222@gmail.com>
2021-08-03 15:55:18 +0100

    Finished this lesson

---
+++ java/learning/src/com/yusuf/learning/copyobjects/CopyObjects.java
@@ -0,0 +1,21 @@
+package com.yusuf.learning.copyobjects;
+
+public class CopyObjects {
+    public static void main(String[] args) {
+        Car car1 = new Car("Tesla", "Y",2021);
+        //Car car2 = new Car("Tesla", "X",2021);
+        //car2.copy(car1);
+        Car car2 = new Car(car1);
+
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println();
+        System.out.println(car1.getMake());
+        System.out.println(car1.getModel());
+        System.out.println(car1.getYear());
+        System.out.println();
+        System.out.println(car2.getMake());
+        System.out.println(car2.getModel());
+        System.out.println(car2.getYear());
+    }
+}
