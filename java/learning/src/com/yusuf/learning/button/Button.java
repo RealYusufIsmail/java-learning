ff637c349fda88ae22c9694e2d94fd99fcd4dff4
YusufIsmail <yusufgamer222@gmail.com>
2021-08-08 11:37:42 +0100

    Finished this lesson

--- java/learning/src/com/yusuf/learning/button/Button.java
+++ java/learning/src/com/yusuf/learning/button/Button.java
@@ -2,6 +2,8 @@ package com.yusuf.learning.button;
 
 public class Button {
     public static void main(String[] args) {
+        // JButton = a button that performs an action when clicked on
 
+        new MyFrame();
     }
 }
