598a89721504043e2a081b95db294d9b91eb35f4
Yusuf <yusufgamer222@gmail.com>
2021-12-12 11:25:59 +0000

    fixed

--- java17/src/main/java/learning/json/Minecraft.java
+++ java17/src/main/java/learning/json/Minecraft.java
@@ -1,47 +1,63 @@
 package learning.json;
 
-import java.util.Map;
+import java.util.List;
 
 public class Minecraft {
-  private String item;
-  private String nbt;
-  private String result;
-  private Map<String, String> enchantments; // map
+  String item;
+  String result;
+  List<String> test;
+  Test2 test2;
 
-
-  String setItem(String item) {
+  public void setItem(String item) {
     this.item = item;
-    return item;
   }
 
-  String setNbt(String nbt) {
-    this.nbt = nbt;
-
-    return nbt;
-  }
-
-
-  private void setEnchantments(Map<String, String> enchantments) {
-    this.enchantments = enchantments;
-  }
-
-
-  public void setRecipe(String item, String nbt) {
-    setRecipe(item, nbt);
-  }
-
-  public void setRecipe(String item, String nbt, String result) {
-    setRecipe(item, nbt);
-    setResult(result);
-  }
-
-  public void setRecipe(String item, String nbt, String result, Map<String, String> enchantments) {
-    setRecipe(item, nbt);
-    setResult(result);
-    setEnchantments(enchantments);
-  }
-
-  private void setResult(String result) {
+  public void setResult(String result) {
     this.result = result;
+  }
+
+  public void setNbt(List<String> test) {
+    this.test = test;
+  }
+
+  public void setTest2(Test2 test2) {
+    this.test2 = test2;
+  }
+}
+
+
+class Test2 {
+  Baz test3;
+
+  public void setBaz(Baz test3) {
+    this.test3 = test3;
+  }
+}
+
+
+class Baz {
+  List<Foobar> test5;
+  int HideFlags;
+
+  public void setFoobar(List<Foobar> foobar) {
+    this.test5 = foobar;
+  }
+
+  public void setHideFlags(int HideFlags) {
+    this.HideFlags = HideFlags;
+  }
+}
+
+
+class Foobar {
+  String id;
+  int lvl;
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public void setLvl(int lvl) {
+    this.lvl = lvl;
   }
 }
--- java17/src/main/java/learning/json/MinecraftObject.java
+++ java17/src/main/java/learning/json/MinecraftObject.java
@@ -6,12 +6,17 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MinecraftObject {
   private static final String ITEM = "item";
-  private static final String NBT = "nbt";
+  private static final ArrayList<String> NBT =
+      new ArrayList<>(Arrays.asList("display", "ench", "AttributeModifiers"));
+  private static final String RESULT = "result";
 
-public static void main(String[] args) {
+  public static void main(String[] args) {
 
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
@@ -27,7 +32,30 @@ public static void main(String[] args) {
 
   public static @NotNull Minecraft serialize() {
     var minecraft = new Minecraft();
-    minecraft.setRecipe(ITEM, NBT);
+    minecraft.setItem(ITEM);
+    minecraft.setResult(RESULT);
+    minecraft.setNbt(NBT);
+    minecraft.setTest2(serializeTest2());
+    return minecraft;
+  }
+
+  public static @NotNull Test2 serializeTest2() {
+    var minecraft = new Test2();
+    minecraft.setBaz(serializeBaz());
+    return minecraft;
+  }
+
+  public static @NotNull Baz serializeBaz() {
+    var minecraft = new Baz();
+    minecraft.setFoobar(List.of(serializeFoo()));
+    minecraft.setHideFlags(1);
+    return minecraft;
+  }
+
+  public static @NotNull Foobar serializeFoo() {
+    var minecraft = new Foobar();
+    minecraft.setId("1");
+    minecraft.setLvl(1);
     return minecraft;
   }
 }
