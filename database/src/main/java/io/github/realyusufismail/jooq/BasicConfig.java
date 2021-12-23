707a0207ba03d5ca34d186be517a594a17b1f868
Yusuf <yusufgamer222@gmail.com>
2021-12-23 15:29:19 +0000

    improving

---
+++ database/src/main/java/io/github/realyusufismail/jooq/BasicConfig.java
@@ -0,0 +1,14 @@
+package io.github.realyusufismail.jooq;
+
+import io.github.yusufsdiscordbot.config.Config;
+
+enum BasicConfig {
+    ;
+    static String userName() {
+        return Config.getString("USERNAME");
+    }
+
+    static String password() {
+        return Config.getString("PASSWORD");
+    }
+}
--- database/src/main/java/io/github/realyusufismail/jooq/DataBase.java
+++ database/src/main/java/io/github/realyusufismail/jooq/DataBase.java
@@ -1,5 +1,37 @@
 package io.github.realyusufismail.jooq;
 
-public final class DataBase {
+import ch.qos.logback.classic.Logger;
+import io.github.yusufsdiscordbot.config.Config;
+import jooq.public_.tables.Users;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.impl.DSL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static jooq.public_.Tables.USERS;
+
+public class DataBase {
+  private static final Logger LOG = (Logger) org.slf4j.LoggerFactory.getLogger(DataBase.class);
+
+  public static void main(String[] args) {
+    try (Connection connection =
+        DriverManager.getConnection("jdbc:h2:file:/Users/YusufIsmail/OneDrive/sqldb/sql",
+                BasicConfig.userName(),BasicConfig.password())) {
+      DSLContext create = DSL.using(connection);
+
+      // language=h2
+      Result<Record> records = create.fetch("select id, email from users");
+      for (Record record : records) {
+        String email = record.get(USERS.EMAIL);
+        Integer id = record.get(USERS.ID);
+        LOG.info("'{}'" + " " + "'{}'", id, email);
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
 }
