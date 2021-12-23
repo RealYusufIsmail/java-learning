1662e11b3714ce3629b11dff13182b95548c3650
Yusuf <yusufgamer222@gmail.com>
2021-12-23 22:07:24 +0000

    added a fix which will be replaced

--- database/src/main/java/io/github/realyusufismail/jooq/BasicConfig.java
+++ database/src/main/java/io/github/realyusufismail/jooq/BasicConfig.java
@@ -3,12 +3,12 @@ package io.github.realyusufismail.jooq;
 import io.github.yusufsdiscordbot.config.Config;
 
 enum BasicConfig {
-    ;
-    static String userName() {
-        return Config.getString("USERNAME");
-    }
+  ;
+  static String userName() {
+    return Config.getString("USERNAME");
+  }
 
-    static String password() {
-        return Config.getString("PASSWORD");
-    }
+  static String password() {
+    return Config.getString("PASSWORD");
+  }
 }
--- database/src/main/java/io/github/realyusufismail/jooq/DataBase.java
+++ database/src/main/java/io/github/realyusufismail/jooq/DataBase.java
@@ -1,8 +1,6 @@
 package io.github.realyusufismail.jooq;
 
 import ch.qos.logback.classic.Logger;
-import io.github.yusufsdiscordbot.config.Config;
-import jooq.public_.tables.Users;
 import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.Result;
@@ -12,15 +10,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import static jooq.public_.Tables.USERS;
 
 public class DataBase {
   private static final Logger LOG = (Logger) org.slf4j.LoggerFactory.getLogger(DataBase.class);
 
   public static void main(String[] args) {
-    try (Connection connection =
-        DriverManager.getConnection("jdbc:h2:file:/Users/YusufIsmail/OneDrive/sqldb/sql",
-                BasicConfig.userName(),BasicConfig.password())) {
+    try (Connection connection = DriverManager.getConnection("jdbc:h2:./database/build/database",
+        BasicConfig.userName(), BasicConfig.password())) {
       DSLContext create = DSL.using(connection);
 
       // language=h2
